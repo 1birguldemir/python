1925b9dec451f34bd0c8be084a143f572e69b25f
Birgül Demir <birguldemirx@gmail.com>
2025-08-07 10:53:45 +0300

    Alıştırma1

---
+++ ödevler/odev1/alistirma_1.py
@@ -0,0 +1,25 @@
+print("*************************")
+sayi = int(input("Lütfen bir sayi giriniz."))
+if sayi % 2 ==0:
+    print("Bu sayı çifttir")
+else :
+    print("Bu sayı tektir")
+
+print("*************************")
+s1 = int(input("1. sayıyı giriniz:"))
+s2 = int(input("2. sayıyı giriniz:"))
+print("Sayıların toplamı: ",s1+s2)
+
+
+print("*************************")
+str = "Birtgül Demir"
+print(len(str))
+
+print("*************************")
+str = "Birgül Demir"
+print(str[::-1])
+
+print("*************************")
+str = "Bugün hava çok güzel."
+kelimeler = str.split();
+print(len(kelimeler))
