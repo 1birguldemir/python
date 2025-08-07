b90dfd3898a1311256568a8c65f32532893f6152
Birgül Demir <birguldemirx@gmail.com>
2025-08-07 16:39:03 +0300

    5.DERS

---
+++ 5.ders/if_else.py
@@ -0,0 +1,55 @@
+
+# sayi =20
+# if sayi == 20:
+#     print(sayi)
+
+
+# if sayi==30:
+#     print(sayi)
+
+
+# yas = int(input("yaşınızı giriniz:"))
+# if yas<18:
+#     print("giremezsiniz.")
+# else:
+#     print("hoşgeldiniz")
+
+# sayi = int(input("sayiyi giriniz: "))
+# if sayi<0:
+#     print("negatif sayı.") 
+
+#if - elif - else
+# islem = int(input("işlem seçiniz:"))
+
+# if islem ==1:
+#     print("1.işlem seçildi")
+# elif islem == 2:
+#     print("2. işlem seçildi")
+# elif islem ==3:
+#     print("3.işlem seçildi")
+# else:
+#     print("geçersiz işlem...")
+
+
+# yas = int(input("yaşınızı giriniz: "))
+
+# if yas>7 and yas<12:
+#     print("ilkokuldasınız")
+# if yas>11 and yas<16:
+#     print("ortaokuldasınız")
+# if yas>15 and yas<19:
+#     print("lisedesiniz")
+# if yas>18 and yas<23:
+#     print("üniversitedesiniz")
+
+
+note = float(input("notunuzu giriniz:"))
+
+if note>=90:
+    print("AA")
+elif note>=75:
+    print("BA")
+elif note>=50:
+    print("BB")
+else:
+    print("SINIFTRA KALDINIZ!")
---
+++ 5.ders/mantiksal_baglaclar.py
@@ -0,0 +1,10 @@
+#and
+print(1<2 and "selçuk"=="selçuk")
+
+#or
+print(1>2 or "selçuk"=="selçuk")
+
+#not operatorü
+print(not "python"=="python")
+
+print(not (2.14>3.19 or (2!=2 and "Selçuk" == "Selçuk")))
