562ed8687a15e6548a6af73e64b94bb7323e36f7
Birgül Demir <birguldemirx@gmail.com>
2025-08-08 13:33:42 +0300

    6. ders

---
+++ 6. ders/while.py
@@ -0,0 +1,12 @@
+# i = 0
+# while i<10:
+#     print("i'nin değeri: ",i)
+#     i+=1
+
+
+# liste = ["elma","armut","ü<üm","incir","kivi"]
+# a=0
+# while a< len(liste):
+#     print("ındex: ",a, "eleman:" ,liste[a])
+#     a+=1
+
